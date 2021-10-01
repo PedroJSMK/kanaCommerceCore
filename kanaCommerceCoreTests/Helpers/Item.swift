71f29095a2cdf54b9bc987976ca67f9356e32076
Pedro Marques <pedrokanagusto@hotmail.com>
2021-10-01 08:08:17 -0300

    UseCaseTest

---
+++ kanaCommerceCoreTests/Helpers/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  kanaCommerceCoreTests
+//
+//  Created by PedroJSMK on 01/10/21.
+//
+
+import Foundation
+
+import kanaCommerceCore
+
+struct Item: ItemProtocol {
+    var id: Int
+    var name: String
+    var price: Double
+}
